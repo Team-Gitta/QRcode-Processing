b4747102d208f345270a95e2c325ce2f3299ea17
seoh02h <seok02h@khu.ac.kr>
2020-11-25 01:00:28 +0900

    UPDATE with serial connection

--- detect_and_decode_QRcodes_live.py
+++ detect_and_decode_QRcodes_live.py
@@ -1,4 +1,15 @@
 import cv2
+import math
+import serial
+import time
+ser = serial.Serial('/dev/ttyAMA0',115200)
+ser.timeout = 100
+def dist(point1, point2):
+	return math.sqrt(pow(point1[0]-point2[0],2)+pow(point1[1]-point2[1],2))
+
+pos = ['bottom','left','top','right']
+left=0
+right=0
 # initalize the cam
 cap = cv2.VideoCapture(0)
 # initialize the cv2 QRCode detector
@@ -15,13 +26,29 @@ while True:
                 point1 = tuple(bbox[0][i])
                 point2 = tuple(bbox[0][(i+1) % 4])
                 cv2.line(img, point1, point2, color=(255, 0, 0), thickness=2)
-
+                print(pos[i],": ",dist(point1,point2))
+                if(i==1):
+                    left = dist(point1,point2)
+                if(i==3):
+                    right = dist(point1,point2)
+                    print(left/right) # left : 1.1 right : 0.0
             if data:
                 print("[+] QR Code detected, data:", data)
+                if data == 'seoo':
+                    start = time.time()
+                    key = '8'
+                    ser.write(key)
+                    while 1:
+                        now = time.time()
+                        if now > start+5:
+                            key = '7'
+                            ser.write(key)
+                            break
+                    break
         # display the result
-        cv2.imshow("img", img)    
-        if cv2.waitKey(1) == ord("q"):
-            break
+        #cv2.imshow("img", img)    
+        #if cv2.waitKey(1) == ord("q"):
+            # break
     except Exception as e:
         print("err:", str(e))
 cap.release()
